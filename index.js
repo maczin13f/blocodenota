be954e1415b15b7d384a36ccacbef18acc27efbb
maczin13f <154028448+maczin13f@users.noreply.github.com>
2025-08-07 17:40:00 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const inputTitulo = document.getElementById('tituloNota');
 const inputNota = document.getElementById('inputDeNotas');
+const notasSalvas = document.querySelector('.notasSalvas');
+const notasCaixa = document.querySelector('.notasCaixa');
 
 function salvarTitulo() {
+        notasSalvas.innerHTML = '<p><b>Suas Notas serao salvas no campo abaixo</b></p>';
+
     const pegaTituloLocal = JSON.parse(localStorage.getItem('titulolocal')) || [];
     
     pegaTituloLocal.push(inputTitulo.value);
@@ -12,14 +16,25 @@ function salvarTitulo() {
 }
 
 function salvaNotaInteira() {
-    const pegaNotasInteira = JSON.parse(localStorage.getItem('notasLocalInteira')) || [];
+    const pegaNotas = JSON.parse(localStorage.getItem('notasLocalInteira')) || [];
 
-    pegaNotasInteira.push(inputTitulo.value);
-    pegaNotasInteira.push(inputNota.value);
+    pegaNotas.push(inputNota.value);
 
-    const salvarNotasInteira = localStorage.setItem('notasLocalInteira', JSON.stringify(pegaNotasInteira));
+    const pegaTitulo = JSON.parse(localStorage.getItem('tituloLocalInteiro')) || [];
 
-    inputNota.value = ''
+    pegaNotas.push(inputTitulo.value);
+
+    const salvaTitulo = localStorage.setItem('tituloLocalInteiro', JSON.stringify(pegaTitulo))
+
+    const salvarNotasInteira = localStorage.setItem('notasLocalInteira', JSON.stringify(pegaNotas));
+
+    notasCaixa.innerHTML = `
+    <p id='titulo'>${inputTitulo.value}<p>
+    <textarea id='nota value='${inputNota.value}'></textarea>
+    `
+
+    inputNota.value = '';
+    inputTitulo.value = '';
 }
 
 function carregarNotas() {
@@ -41,8 +56,6 @@ salvarTitulo();
 carregarNotas()
 }})
 
-window.onload = carregarNotas()
-
 function salvarNota() {
     const pegaNotaLocal = JSON.parse(localStorage.getItem('notalocal')) || [];
     
@@ -63,5 +76,16 @@ function apagaNotas() {
     localStorage.removeItem('titulolocal');
     window.location.reload()
 }
+
+document.body.addEventListener('keydown', function(event) {
+    if (event.ctrlKey && event.key.toLowerCase() === 'l') {
+        event.preventDefault();
+        apagaNotas();
+    }
+});
+
+
+window.onload = carregarNotas()
+
 
 
